f1123f43b3df9e1e3d0a190f9ef7548d0dd0e81b
Adamb710 <adamuerj@gmail.com>
2025-08-26 01:19:07 +0000

    Atualizado comparaçao de atributos das cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,7 @@ int main(){   //Função principal
    int pontos_turisticos1;       // Número inteiro para quantidade de pontos turísticos
    float densidade1;             // Variável calculada: densidade populacional (hab/km²)
    float pibpercapita1;          // Variável calculada: PIB per capita em reais
+   double superpoder1;
 
  //Declaração de variaveis da segunda carta
 
@@ -26,7 +27,8 @@ int main(){   //Função principal
    int pontos_turisticos2;
    float densidade2;
    float pibpercapita2;
-
+   double superpoder2;
+  
    // Exibição das instruções do jogo para o usuário
    printf("**** SUPER TRUNFO - CADASTRO DE CARTAS*****\n\n");
    printf("Digite abaixo as informações para cadastrar suas cartas.\n\n");
@@ -91,6 +93,8 @@ int main(){   //Função principal
  
    densidade1 = (float) populacao1 / area1;  // Converte populacao1 para float e calcula densidade
    pibpercapita1 = (float) (pib1 * 1000000000) / populacao1;   // Converte PIB de bilhões para unidade e divide pela população
+   superpoder1 = (double) populacao1 + area1 +pib1 + pontos_turisticos1 + (1 / densidade1);  
+   superpoder2 = (double) populacao2 + area2 +pib2 + pontos_turisticos2 + (1 / densidade2); 
 
  // Exibição da primeira carta
 
@@ -103,7 +107,8 @@ int main(){   //Função principal
    printf("PIB: %.2f bilhões de reais\n", pib1);  // PIB formatado com 2 casas decimais
    printf("Numero de pontos turisticos: %d\n", pontos_turisticos1); // Número de pontos turísticos
    printf("Densidade Populacional: %.2f Hab/km²\n",densidade1);     // Densidade com 2 casas decimais
-   printf ("PIB per capita: %.2f reais\n\n", pibpercapita1);        // PIB per capita com 2 casas decimais
+   printf ("PIB per capita: %.2f reais\n", pibpercapita1);        // PIB per capita com 2 casas decimais
+   printf("Super poder (soma dos atributos): %.2f\n\n", superpoder1);
 
 
      densidade2 = (float) populacao2 / area2; 
@@ -121,9 +126,43 @@ int main(){   //Função principal
    printf("Numero de pontos turisticos: %d\n", pontos_turisticos2);
    printf("Densidade Populacional: %.2f Hab/km²\n", densidade2);
    printf ("PIB per capita: %.2f reais\n", pibpercapita2);
+   printf("Super poder (soma dos atributos): %.2f\n\n", superpoder2);
+
+
+   printf("---- Hora da batalha ----\n\n");
+   printf(" Nesta etapa as cartes irão duelar, seus atributos serão comparados e os maiores\n");
+   printf("atributos vencem, a carta com o maior atributo somado sera a grande campeã\n\n");
+
+  int resultadopopulcao;
+  int resultadoarea;
+  int resultadoPIB;
+  int resultadoturisticos;
+  int resultadodensidade;
+  int resultadoiPIBpercapita;
+  int resultadosuperpoder;
+
+resultadopopulcao = populacao1 > populacao2;
+resultadoarea = area1 > area2;
+resultadoPIB = pib1 > pib2;
+resultadoturisticos = pontos_turisticos1 > pontos_turisticos2;
+resultadodensidade = densidade1 > densidade2;
+resultadoiPIBpercapita = pibpercapita1 > pibpercapita2;
+resultadosuperpoder = superpoder1 > superpoder2;
+
+//Foram utilizado operadores ternarios (? :) abaixo para imprimir duas opçoes de string carta 1 vencer e carta 2 venceu.
+//Se o resultado for 1 verdadero exibe carta 1 venceu, se for 0 falso exibe carta 2 venceu.
+
+printf("Resultado do duelo:\n\n");
+printf("População: %s (Resultado: %d)\n", resultadopopulcao ? "Carta 1 venceu" : "Carta 2 venceu", resultadopopulcao);
+printf("Area: %s (Resultado: %d)\n", resultadoarea ? "Carta 1 venceu" : "Carta 2 venceu", resultadoarea);
+printf("PIB: %s (Resultado: %d)\n", resultadoPIB ? "Carta 1 venceu" : "Carta 2 venceu", resultadoPIB);
+printf("Pontos turisticos: %s (Resultado: %d)\n", resultadoturisticos ? "Carta 1 venceu" : "Carta 2 venceu", resultadoturisticos);
+printf("Densidade: %s (Resultado: %d)\n", resultadodensidade ? "Carta 1 venceu" : "Carta 2 venceu", resultadodensidade);
+printf("PIB Per capita: %s (Resultado: %d)\n", resultadoiPIBpercapita ? "Carta 1 venceu" : "Carta 2 venceu", resultadoiPIBpercapita);
+printf("Super Poder: %s (Resultado: %d)\n", resultadosuperpoder ? "Carta 1 venceu" : "Carta 2 venceu", resultadosuperpoder);
 
 
 
-   return 0;   //Retorna 0 para indicar que o programa executou com sucesso
+return 0;   //Retorna 0 para indicar que o programa executou com sucesso
 
 }
